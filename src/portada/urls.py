633384631ddb99e999d2a0115fbfa06f0b2303a0
Herald Olivares <heraldo@jorge-HP-Compaq-8200-Elite-SFF-PC.(none)>
2013-06-19 18:33:30 -0500

    fixes

--- src/portada/urls.py
+++ src/portada/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.defaults import patterns, url
 from .views import PortadaListView
 
 urlpatterns = patterns('',
-    url(r'^portadas/$', PortadaListView.as_view(),
+    url(r'^$', PortadaListView.as_view(),
         name='portafolio_albumes'),    
 )
